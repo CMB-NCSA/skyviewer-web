aa2b1c0d48cfc2a3663ee15a639c59785cf203d8
Felipe Menanteau <felipe@illinois.edu>
2023-08-15 14:38:51 -0500

    Fix typo

--- split-view/js/app.js
+++ split-view/js/app.js
@@ -35,9 +35,6 @@ A.init.then(() => {
   a1.setFovRange(0.01, 175);
   a1.createImageSurvey("NCSA/P/DES/ColorIRG", 'DES/NCSA DR2 ColorIRG', hipsDirDESDR2, 'equatorial', 11, {imgFormat: 'png'});
   a1.createImageSurvey('P/PLANCK/R3/HFI/color', 'PLANCK R2 HFI color composition 353-545-857 GHz', 'https://alasky.cds.unistra.fr/PLANCK/R3/HFI_Color_353_545_857/', 'galactic', 3, {imgFormat: 'jpeg'});
-  hipsDir, 'galactic', 3, {imgFormat: 'png'}));
-
-
   a1.setBaseImageLayer("NCSA/P/DES/ColorIRG");
 
   a2 = A.aladin('#al2', {
